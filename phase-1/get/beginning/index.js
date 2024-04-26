f45a4bb396b3b7bd27d5159130cefa964c4ab356
John John Penafiel <johnjohnpenafiel@hotmail.com>
2024-04-26 15:02:42 -0400

    end of day april 26 commit

--- phase-1/get/beginning/index.js
+++ phase-1/get/beginning/index.js
@@ -16,13 +16,38 @@ fetch("https://dog-api.kinduff.com/api/facts?number=1")
 // Convert the JSON reuqest to JS.
 .then((response) => response.json())
 // Use the resulting JS!
-.then((data) => {console.log(data.facts[0])})
+.then((data) => {
+    const span =document.querySelector("#dog");
+    span.textContent = data.facts[0];
+    // DOM mod decition tree
+
+})
 .catch((error) => {console.log(error)});
 
 
 // A synchronous request to https://anapioficeandfire.com/api/books.
+fetch('https://anapioficeandfire.com/api/books')
+.then(response => response.json())
+.then(books => {
+    const got = document.querySelector('#got');
+    books.forEach((book) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${book['name']} (${book.NumberOfPages})`;
+        got.append(listItem);
+    });
+})
+.catch((error) => {console.log(error)});
+
 
 // An asynchronous request to https://pokeapi.co/api/v2/pokemon/[name]"
 
 // ~ Challenge: Make a GET request to an API of your choice!
+/*
+fetch("https://api.thedogapi.com/v1/images/search")
+.then(response => response.json())
+.then(images => {
+    const dogPics = document.body;
+
+
+}
 
